14806bc0b9e0024a2ca0218f74ddb5bc89993c59
ddhuissier <ddhuissier@gmail.com>
2021-08-16 13:15:43 +0200

    First commit

---
+++ ConsoleConnectToApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.Http;
+
+using ConsoleConnectToApi;
+
+swaggerClient client = new("https://localhost:44352/", new HttpClient());
+var results = await client.WeatherForecastAsync();
+foreach (var result in results)
+{
+    Console.WriteLine($"Summary: {result.Summary}");
+}
+
